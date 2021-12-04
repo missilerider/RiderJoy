86a771c801eb27afb7153a02367fa532f94c4492
DESKTOP-BVVMJSS\alfonso <alfonso.vila.lopez@gmail.com>
2021-12-04 01:16:22 +0100

    Inicio MUX

--- src/config_global.h
+++ src/config_global.h
@@ -14,3 +14,25 @@
 /** Milisegundos a esperar para cancelar la aceleración de un rotary axis
  */
 #define ROTARY_IDLE_TIMEOUT     20;
+
+
+/** Se puede conectar un multiplexor ("MUX") de puertos analógicos (74HC4067) para 
+ * utilizar 16 entradas analógicas en un solo pin analógico del Arduino (MUX_PIN_IN).
+ * El posible conectar múltiples
+ * 
+ * Conexión:
+ * MUX      Arduino
+ * S0       MUX_ADDR_PIN0
+ * S1       MUX_ADDR_PIN1
+ * S2       MUX_ADDR_PIN2
+ * S3       MUX_ADDR_PIN3
+ * SIG      Pin analógico
+ * VCC      VCC
+ * GND      GND
+ * 
+ * El pin analógico puede ser único
+ */
+#define MUX_ADDR_PIN0   15
+#define MUX_ADDR_PIN1   14
+#define MUX_ADDR_PIN2   16
+#define MUX_ADDR_PIN3   10
